2a47a57fe134cca00462917b4806344d167226d8
Josh Bailey <josh.bailey@uga.edu>
2024-12-10 12:17:08 -0500

    got static and templates setup

--- info/views.py
+++ info/views.py
@@ -5,4 +5,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = { }
+    return render(request, 'info/index.html', context=context)
